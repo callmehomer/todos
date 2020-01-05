9ade7c8a9d4cd9b89e06d2ff49d487005a6b1c95
Kamil Homernik <31961734+callmehomer@users.noreply.github.com>
2020-01-05 15:41:29 +0100

    podzial na fe i be

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const mysql = require('mysql')
-const router = express.Router()
+const cors = require('cors');
+const path = require('path');
+
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const pass = "password"
 
@@ -16,44 +24,55 @@ function getConnection() {
 
 const conn = getConnection()
 
-router.get('/', (req, res) => {
-    res.render('index')
-})
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
 
-router.get('/get_todos', (req, res) => {
+app.get('/frontend/', (req, res) => {
+    res.send(path.join('/frontend/index.html'));
+    res.sendFile(path.join('/frontend/index.html'));
+
+});
+
+app.get('/get_todos', (req, res) => {
     const queryString = "SELECT * FROM todos WHERE complete = '0'"
     conn.query(queryString, (err, rows, fields) => {
         if (err) {
             console.log("Failed to query @ /get_todo: " + err)
+            return null
         }
         console.log("Getting data from database @ /get_todos")
         res.json(rows)
     })
 })
 
-router.post('/add_todo', (req, res) => {
+app.post('/add_todo', (req, res) => {
     console.log(req.body)
     const todo = req.body.add_todo_input
     const queryString = "INSERT INTO todos (todo) VALUES (?)"
     conn.query(queryString, [todo], (err, row, fields) => {
         if (err) {
             console.log("Failed to insert @ /add_todo: " + todo + " " + err)
+            return null
         }
         console.log("@/add_todo : " + todo + " added.")
         res.redirect('/')
     })
 })
 
-router.post('/complete_todo/:id', (req,res) => {
+app.post('/complete_todo/:id', (req,res) => {
     const todo_id = req.params.id
     const queryString = "UPDATE todos SET complete ='1' WHERE todo_id = ?"
     conn.query(queryString, [todo_id], (err, rows, fields) => {
         if(err){
             console.log("Failed to complete todo @ /complete_todo: " + todo_id + " " + err)
+            return null
         }
         console.log("@/complete_todo/ completing todo with id " + todo_id)
         res.redirect('/')
     })
 })
 
-module.exports = router
+app.listen(5500, () => {
+    console.log('App running at http://localhost:5500')
+})
